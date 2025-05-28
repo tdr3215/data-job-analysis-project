9dd52f2758492dc136753db37f4ac1896758cac6
Tiffany Rose <tiffany.drose@gmail.com>
2025-05-28 11:50:03 -0400

    finished querying for skills of top paying jobs in 2023

---
+++ project_sql/2_top_paying_job_skills.sql
@@ -0,0 +1,435 @@
+/*
+Q: What skills are required for the top paying Data/Business Analyst roles?
+- Use the top 10 highest-paying roles from the first query
+- Add specific skills required for these roles
+- Why? Being aware of the top skills in the industry will help
+job-seekers plan out their skill set to align with higher paying job oppurtunities 
+*/
+
+-- Top Paying Jobs
+
+WITH top_paying_jobs AS (
+    SELECT
+        job_id,
+        job_title,
+        job_location,
+        job_schedule_type,
+        salary_year_avg,
+        job_posted_date,
+        name AS company_name
+    FROM
+        job_postings_fact
+        LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
+    WHERE
+        job_title_short in ('Data Analyst', 'Business Analyst')
+        AND salary_year_avg IS NOT NULL
+        AND job_location in ('Anywhere', 'Atlanta, GA', 'Georgia')
+    ORDER BY
+        salary_year_avg DESC
+    LIMIT
+        10
+)
+SELECT 
+    top_paying_jobs.*,
+    skills
+FROM top_paying_jobs
+JOIN skills_job_dim ON top_paying_jobs.job_id = skills_job_dim.job_id
+JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+ORDER BY salary_year_avg
+;
+
+/*
+Result:
+[
+  {
+    "job_id": 168310,
+    "job_title": "Principal Data Analyst (Remote)",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "205000.0",
+    "job_posted_date": "2023-08-09 11:00:01",
+    "company_name": "SmartAsset",
+    "skills": "sql"
+  },
+  {
+    "job_id": 168310,
+    "job_title": "Principal Data Analyst (Remote)",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "205000.0",
+    "job_posted_date": "2023-08-09 11:00:01",
+    "company_name": "SmartAsset",
+    "skills": "gitlab"
+  },
+  {
+    "job_id": 168310,
+    "job_title": "Principal Data Analyst (Remote)",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "205000.0",
+    "job_posted_date": "2023-08-09 11:00:01",
+    "company_name": "SmartAsset",
+    "skills": "tableau"
+  },
+  {
+    "job_id": 168310,
+    "job_title": "Principal Data Analyst (Remote)",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "205000.0",
+    "job_posted_date": "2023-08-09 11:00:01",
+    "company_name": "SmartAsset",
+    "skills": "excel"
+  },
+  {
+    "job_id": 168310,
+    "job_title": "Principal Data Analyst (Remote)",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "205000.0",
+    "job_posted_date": "2023-08-09 11:00:01",
+    "company_name": "SmartAsset",
+    "skills": "numpy"
+  },
+  {
+    "job_id": 168310,
+    "job_title": "Principal Data Analyst (Remote)",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "205000.0",
+    "job_posted_date": "2023-08-09 11:00:01",
+    "company_name": "SmartAsset",
+    "skills": "pandas"
+  },
+  {
+    "job_id": 168310,
+    "job_title": "Principal Data Analyst (Remote)",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "205000.0",
+    "job_posted_date": "2023-08-09 11:00:01",
+    "company_name": "SmartAsset",
+    "skills": "snowflake"
+  },
+  {
+    "job_id": 168310,
+    "job_title": "Principal Data Analyst (Remote)",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "205000.0",
+    "job_posted_date": "2023-08-09 11:00:01",
+    "company_name": "SmartAsset",
+    "skills": "go"
+  },
+  {
+    "job_id": 168310,
+    "job_title": "Principal Data Analyst (Remote)",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "205000.0",
+    "job_posted_date": "2023-08-09 11:00:01",
+    "company_name": "SmartAsset",
+    "skills": "python"
+  },
+  {
+    "job_id": 112859,
+    "job_title": "Manager II, Applied Science - Marketplace Dynamics",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "214500.0",
+    "job_posted_date": "2023-12-18 08:02:37",
+    "company_name": "Uber",
+    "skills": "python"
+  },
+  {
+    "job_id": 1021647,
+    "job_title": "Data Analyst (Hybrid/Remote)",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "217000.0",
+    "job_posted_date": "2023-01-17 00:17:23",
+    "company_name": "Uclahealthcareers",
+    "skills": "crystal"
+  },
+  {
+    "job_id": 1021647,
+    "job_title": "Data Analyst (Hybrid/Remote)",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "217000.0",
+    "job_posted_date": "2023-01-17 00:17:23",
+    "company_name": "Uclahealthcareers",
+    "skills": "flow"
+  },
+  {
+    "job_id": 1021647,
+    "job_title": "Data Analyst (Hybrid/Remote)",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "217000.0",
+    "job_posted_date": "2023-01-17 00:17:23",
+    "company_name": "Uclahealthcareers",
+    "skills": "tableau"
+  },
+  {
+    "job_id": 1021647,
+    "job_title": "Data Analyst (Hybrid/Remote)",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "217000.0",
+    "job_posted_date": "2023-01-17 00:17:23",
+    "company_name": "Uclahealthcareers",
+    "skills": "oracle"
+  },
+  {
+    "job_id": 1021647,
+    "job_title": "Data Analyst (Hybrid/Remote)",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "217000.0",
+    "job_posted_date": "2023-01-17 00:17:23",
+    "company_name": "Uclahealthcareers",
+    "skills": "sql"
+  },
+  {
+    "job_id": 502610,
+    "job_title": "Lead Business Intelligence Engineer",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "220000.0",
+    "job_posted_date": "2023-08-29 18:26:36",
+    "company_name": "Noom",
+    "skills": "python"
+  },
+  {
+    "job_id": 502610,
+    "job_title": "Lead Business Intelligence Engineer",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "220000.0",
+    "job_posted_date": "2023-08-29 18:26:36",
+    "company_name": "Noom",
+    "skills": "sql"
+  },
+  {
+    "job_id": 502610,
+    "job_title": "Lead Business Intelligence Engineer",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "220000.0",
+    "job_posted_date": "2023-08-29 18:26:36",
+    "company_name": "Noom",
+    "skills": "excel"
+  },
+  {
+    "job_id": 502610,
+    "job_title": "Lead Business Intelligence Engineer",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "220000.0",
+    "job_posted_date": "2023-08-29 18:26:36",
+    "company_name": "Noom",
+    "skills": "tableau"
+  },
+  {
+    "job_id": 502610,
+    "job_title": "Lead Business Intelligence Engineer",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "220000.0",
+    "job_posted_date": "2023-08-29 18:26:36",
+    "company_name": "Noom",
+    "skills": "looker"
+  },
+  {
+    "job_id": 502610,
+    "job_title": "Lead Business Intelligence Engineer",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "220000.0",
+    "job_posted_date": "2023-08-29 18:26:36",
+    "company_name": "Noom",
+    "skills": "chef"
+  },
+  {
+    "job_id": 99305,
+    "job_title": "Data Analyst, Marketing",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "232423.0",
+    "job_posted_date": "2023-12-05 20:00:40",
+    "company_name": "Pinterest Job Advertisements",
+    "skills": "hadoop"
+  },
+  {
+    "job_id": 99305,
+    "job_title": "Data Analyst, Marketing",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "232423.0",
+    "job_posted_date": "2023-12-05 20:00:40",
+    "company_name": "Pinterest Job Advertisements",
+    "skills": "tableau"
+  },
+  {
+    "job_id": 99305,
+    "job_title": "Data Analyst, Marketing",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "232423.0",
+    "job_posted_date": "2023-12-05 20:00:40",
+    "company_name": "Pinterest Job Advertisements",
+    "skills": "python"
+  },
+  {
+    "job_id": 99305,
+    "job_title": "Data Analyst, Marketing",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "232423.0",
+    "job_posted_date": "2023-12-05 20:00:40",
+    "company_name": "Pinterest Job Advertisements",
+    "skills": "sql"
+  },
+  {
+    "job_id": 99305,
+    "job_title": "Data Analyst, Marketing",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "232423.0",
+    "job_posted_date": "2023-12-05 20:00:40",
+    "company_name": "Pinterest Job Advertisements",
+    "skills": "r"
+  },
+  {
+    "job_id": 552322,
+    "job_title": "Associate Director- Data Insights",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "255829.5",
+    "job_posted_date": "2023-06-18 16:03:12",
+    "company_name": "AT&T",
+    "skills": "tableau"
+  },
+  {
+    "job_id": 552322,
+    "job_title": "Associate Director- Data Insights",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "255829.5",
+    "job_posted_date": "2023-06-18 16:03:12",
+    "company_name": "AT&T",
+    "skills": "excel"
+  },
+  {
+    "job_id": 552322,
+    "job_title": "Associate Director- Data Insights",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "255829.5",
+    "job_posted_date": "2023-06-18 16:03:12",
+    "company_name": "AT&T",
+    "skills": "jupyter"
+  },
+  {
+    "job_id": 552322,
+    "job_title": "Associate Director- Data Insights",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "255829.5",
+    "job_posted_date": "2023-06-18 16:03:12",
+    "company_name": "AT&T",
+    "skills": "pyspark"
+  },
+  {
+    "job_id": 552322,
+    "job_title": "Associate Director- Data Insights",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "255829.5",
+    "job_posted_date": "2023-06-18 16:03:12",
+    "company_name": "AT&T",
+    "skills": "pandas"
+  },
+  {
+    "job_id": 552322,
+    "job_title": "Associate Director- Data Insights",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "255829.5",
+    "job_posted_date": "2023-06-18 16:03:12",
+    "company_name": "AT&T",
+    "skills": "aws"
+  },
+  {
+    "job_id": 552322,
+    "job_title": "Associate Director- Data Insights",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "255829.5",
+    "job_posted_date": "2023-06-18 16:03:12",
+    "company_name": "AT&T",
+    "skills": "sql"
+  },
+  {
+    "job_id": 552322,
+    "job_title": "Associate Director- Data Insights",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "255829.5",
+    "job_posted_date": "2023-06-18 16:03:12",
+    "company_name": "AT&T",
+    "skills": "azure"
+  },
+  {
+    "job_id": 552322,
+    "job_title": "Associate Director- Data Insights",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "255829.5",
+    "job_posted_date": "2023-06-18 16:03:12",
+    "company_name": "AT&T",
+    "skills": "databricks"
+  },
+  {
+    "job_id": 552322,
+    "job_title": "Associate Director- Data Insights",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "255829.5",
+    "job_posted_date": "2023-06-18 16:03:12",
+    "company_name": "AT&T",
+    "skills": "r"
+  },
+  {
+    "job_id": 552322,
+    "job_title": "Associate Director- Data Insights",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "255829.5",
+    "job_posted_date": "2023-06-18 16:03:12",
+    "company_name": "AT&T",
+    "skills": "python"
+  },
+  {
+    "job_id": 552322,
+    "job_title": "Associate Director- Data Insights",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "255829.5",
+    "job_posted_date": "2023-06-18 16:03:12",
+    "company_name": "AT&T",
+    "skills": "powerpoint"
+  },
+  {
+    "job_id": 552322,
+    "job_title": "Associate Director- Data Insights",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "255829.5",
+    "job_posted_date": "2023-06-18 16:03:12",
+    "company_name": "AT&T",
+    "skills": "power bi"
+  }
+]
+*/
